12b6d38f460e102621619a920c0cdb54ebdf9dce
pkindalov <panayotkindalov@gmail.com>
2016-02-08 23:42:34 +0200

    Read an integer number from console and write on it square of number stars

---
+++ SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class SquareOfStars
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        string stars = new string('*', N);
+        string empty = new string(' ', N - 2);
+       
+        Console.WriteLine(stars);
+        for(int i = 0; i < N - 2; i++)
+        {
+            Console.WriteLine("*" + empty + "*");
+        }
+        Console.WriteLine(stars);
+
+           
+        }
+    }
