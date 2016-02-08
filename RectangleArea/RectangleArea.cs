3ffe9500913c9c86a03910569abc707789de01dd
pkindalov <panayotkindalov@gmail.com>
2016-02-08 23:37:12 +0200

    Read two numbers from console and write on it area of rectangle

---
+++ RectangleArea/RectangleArea.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class RectangleArea
+{
+    static void Main()
+    {
+        var a = decimal.Parse(Console.ReadLine());
+        var b = decimal.Parse(Console.ReadLine());
+        Console.WriteLine(a*b);
+    }
+}
