a305800d969738e1615e3f922b4c09c12c967fbc
pkindalov <panayotkindalov@gmail.com>
2016-02-08 23:29:06 +0200

    Write on console triangle from 55 stars in 10 rows

---
+++ TriangleOf55Stars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class TriangleOf55Stars
+{
+    static void Main()
+    {
+        for(int a = 0; a <= 10; a++)
+        {
+            string stars = new string('*', a);
+            Console.WriteLine(stars);
+            
+        }
+    }
+}
