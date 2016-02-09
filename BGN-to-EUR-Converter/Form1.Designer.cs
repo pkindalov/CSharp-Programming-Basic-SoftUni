202f8aa6fda0235768103b8bc2a615facf453d3a
pkindalov <panayotkindalov@gmail.com>
2016-02-09 22:27:13 +0200

    BGN To EURO simple convertor(windows forms application) Simple window form application which convert from lev to euro

---
+++ BGN-to-EUR-Converter/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BGN_to_EUR_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.lblTopleftTextConvert = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Location = new System.Drawing.Point(239, 54);
+            this.numericUpDownAmount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDownAmount.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(180, 26);
+            this.numericUpDownAmount.TabIndex = 0;
+            this.numericUpDownAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.ValueChanged += new System.EventHandler(this.numericUpDownAmount_ValueChanged);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.Color.PaleGreen;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(96, 115);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(459, 66);
+            this.labelResult.TabIndex = 1;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTopleftTextConvert
+            // 
+            this.lblTopleftTextConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTopleftTextConvert.Location = new System.Drawing.Point(97, 54);
+            this.lblTopleftTextConvert.Name = "lblTopleftTextConvert";
+            this.lblTopleftTextConvert.Size = new System.Drawing.Size(135, 47);
+            this.lblTopleftTextConvert.TabIndex = 2;
+            this.lblTopleftTextConvert.Text = "Convert";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(426, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 47);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BGN to EUR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 263);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTopleftTextConvert);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BGN to EUR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label lblTopleftTextConvert;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
