cb699763ec3fc781f1887591d7e368c77af69d71
pkindalov <panayotkindalov@gmail.com>
2016-02-09 17:28:41 +0200

    Convert inches to centimeters and write result on console. First variant is on comment. In second variant I use my own methode

---
+++ InchesToCentimeters/InchesToCentimeters.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+
+//class InchesToCentimeters
+//{
+//    static void Main()
+//    {
+//        Console.Write("Inches = ");
+//        var inches = double.Parse(Console.ReadLine());
+//        var centimeters = inches * 2.54;
+//        Console.Write("Centimeters = ");
+//        Console.WriteLine(centimeters);
+//    }
+//}
+
+
+using System;
+
+class InchesToCentimeters
+{
+    static double InchesConvToCm(double inches)
+    {
+        var centimeters = inches * 2.54;
+        return centimeters;
+    }
+    static void Main()
+    {
+        Console.Write("Inches = ");
+        var inches = double.Parse(Console.ReadLine());
+       
+        Console.Write("Centimeters = ");
+        Console.WriteLine(InchesConvToCm(inches));
+    }
+}
