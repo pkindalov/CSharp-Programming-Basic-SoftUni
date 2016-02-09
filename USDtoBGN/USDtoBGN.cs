fe5fd80ed12a5bd3808a21cb74126fdf37671719
pkindalov <panayotkindalov@gmail.com>
2016-02-09 22:48:03 +0200

    Console convertor from USD to Lev

---
+++ USDtoBGN/USDtoBGN.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class USDtoBGN
+{
+    static void Main()
+    {
+        var dollars = double.Parse(Console.ReadLine());
+
+        var convertToBGN = dollars * 1.79549;
+        Console.WriteLine(Math.Round(convertToBGN, 2));
+    }
+}
+
+
