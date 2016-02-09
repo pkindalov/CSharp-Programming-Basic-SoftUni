e75164cca3f45c9a13b548eb50db5c451dbe12aa
pkindalov <panayotkindalov@gmail.com>
2016-02-09 17:24:21 +0200

    Calculate area of a square and write on console. First variant is on comment In second one I have used my own method.

---
+++ Square-Area/Square-Area.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+
+//class SimpleCalculationsExercise
+//{
+//    static void Main()
+//    {
+//        Console.Write("Enter side a of square = ");
+//        var a = double.Parse(Console.ReadLine());
+//        var area = a * a;
+//        Console.Write("Square Area = ");
+//        Console.WriteLine(area);
+
+
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+using System;
+
+class SimpleCalculationsExercise
+{
+    static double SquareAreaCalc(double a)
+    {
+        var area = a * a;
+        return area;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter side a of square = ");
+        var a = double.Parse(Console.ReadLine());
+       
+        Console.Write("Square area = ");
+        Console.WriteLine(SquareAreaCalc(a));
+
+
+    }
+}
