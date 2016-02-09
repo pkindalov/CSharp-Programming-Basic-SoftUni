dbc0e8b75bdd613fe6ac7e17d5a84ccca85f75a0
pkindalov <panayotkindalov@gmail.com>
2016-02-09 17:37:58 +0200

    Read from console your first name , last name, age and then concatenate data in string and write it on console. More simple variant is on comment. In second variant I use my own method

---
+++ Concatenate-Data/Concatenate-Data.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+
+//class ConcatenateData
+//{
+//    static void Main()
+//    {
+//        var firstname = Console.ReadLine();
+//        var lastname = Console.ReadLine();
+//        var age = int.Parse(Console.ReadLine());
+//        var town = Console.ReadLine();
+
+//        Console.WriteLine("You are {0} {1}, a {2}-years old person from {3}.", firstname, lastname, age, town);
+
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+
+class ConcatenateData
+{
+    static string concatenateStrs(string firstname, string lastname, string age, string town)
+    {
+      
+
+        var result = firstname + " " + lastname + " " + age + " " + town;
+        return result;
+    }
+
+    static void Main()
+    {
+        var firstname = Console.ReadLine();
+        var lastname = Console.ReadLine();
+        var age = Console.ReadLine();
+        var town = Console.ReadLine();
+
+        var result = concatenateStrs(firstname, lastname, age, town);
+        Console.WriteLine(result);
+
+        //Console.WriteLine("You are {0} {1}, a {2}-years old person from {3}.", firstname, lastname, age, town);
+
+    }
+}
