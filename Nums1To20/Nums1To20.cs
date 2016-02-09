68bc0180bb4a278d484faa5305287e428f01224e
pkindalov <panayotkindalov@gmail.com>
2016-02-09 17:12:29 +0200

    Write on console numbers from 1 to 20.

---
+++ Nums1To20/Nums1To20.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Nums1To20
+{
+    static void Main()
+    {
+        for(int numbers = 1; numbers <= 20; numbers++)
+        {
+            Console.WriteLine(numbers);
+        }
+        //Console.WriteLine(1);
+        //Console.WriteLine(2);
+        //Console.WriteLine(3);
+        //Console.WriteLine(4);
+        //Console.WriteLine(5);
+        //Console.WriteLine(6);
+        //Console.WriteLine(7);
+        //Console.WriteLine(8);
+        //Console.WriteLine(9);
+        //Console.WriteLine(10);
+        //Console.WriteLine(11);
+        //Console.WriteLine(12);
+        //Console.WriteLine(13);
+        //Console.WriteLine(14);
+        //Console.WriteLine(15);
+        //Console.WriteLine(16);
+        //Console.WriteLine(17);
+        //Console.WriteLine(18);
+        //Console.WriteLine(19);
+        //Console.WriteLine(20);
+
+    }
+}
