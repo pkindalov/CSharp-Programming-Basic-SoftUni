037f315d7d6e85812c90a214a9a2d5e432070925
pkindalov <panayotkindalov@gmail.com>
2016-02-09 18:07:10 +0200

    Read number from console, calculate and write on console circle area and perimeter First way is the simplest and it is on comment. In second way I use one own method. And this variant is on comment. The third variant is a little more complex. I use two my own methods.

---
+++ CircleAreAndPerimeter/CircleAreAndPerimeter.cs
@@ -0,0 +1,71 @@
+﻿//using System;
+
+//class CircleAreAndPerimeter
+//{
+//    static void Main()
+//    {
+//        var r = double.Parse(Console.ReadLine());
+//        var area = Math.PI * r * r;
+//        var perimeter = 2 * Math.PI * r;
+
+//        Console.WriteLine("Area = {0}", area);
+//        Console.WriteLine("Perimeter = {0}", perimeter);
+
+
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+//using System;
+
+//class CircleAreAndPerimeter
+//{
+//    static void circleAreaAndPerim(double r)
+//    {
+//        var area = Math.PI * r * r;
+//        var perimeter = 2 * Math.PI * r;
+//        Console.WriteLine("Area = {0}", area);
+//        Console.WriteLine("Perimeter = {0}", perimeter);
+
+//    }
+
+//    static void Main()
+//    {
+//        var r = double.Parse(Console.ReadLine());
+
+//        circleAreaAndPerim(r);
+
+
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+
+class CircleAreAndPerimeter
+{
+    static double areaCalculate(double r)
+    {
+        var area = Math.PI * r * r;
+        return area;
+    }
+
+    static double perimeterCalculate(double r)
+    {
+        var perimeter = 2 * Math.PI * r;
+        return perimeter;
+    }
+
+    static void Main()
+    {
+        var r = double.Parse(Console.ReadLine());
+       
+        Console.WriteLine("Area = {0}", areaCalculate(r));
+        Console.WriteLine("Perimeter = {0}", perimeterCalculate(r));
+
+
+    }
+}
