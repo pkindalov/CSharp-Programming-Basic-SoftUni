38d553da4e621cded9226787d6819de5f4487b92
pkindalov <panayotkindalov@gmail.com>
2016-02-09 22:44:01 +0200

    Console converter from radians to degrees

---
+++ RadiansToDegrees/RadiansToDegrees.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+
+
+//class RadiansToDegrees
+//{
+//    static void Main()
+//    {
+
+//        var number = double.Parse(Console.ReadLine());
+
+//        var formula = number * (180.0 / Math.PI);
+
+//        Console.WriteLine(Math.Round(formula, 2));
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+
+
+class RadiansToDegrees
+{
+    static double RadToDegConv(double number)
+    {
+        var formula = number * (180.0 / Math.PI);
+        var result = Math.Round(formula, 2);
+        return result;
+    }
+
+    static void Main()
+    {
+
+        var number = double.Parse(Console.ReadLine());
+        var result = RadToDegConv(number);
+
+        Console.WriteLine(result);
+        
+
+      
+    }
+}
