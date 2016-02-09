d9063f18d2a03bca9f2529c22757adb75b646363
pkindalov <panayotkindalov@gmail.com>
2016-02-09 17:32:28 +0200

    Greeting by name. Enter your name and you will be greet on console.

---
+++ Greeting/Greeting.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+
+//class Greeting
+//{
+//    static void Main()
+//    {
+//        //Console.Write("Enter your name: ");
+//        var name = Console.ReadLine();
+//        Console.WriteLine("Hello, {0}!", name);
+
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+class Greeting
+{
+    static void greeting(string name)
+    {
+        Console.WriteLine("Hello, {0}!", name);
+    }
+
+    static void Main()
+    {
+        //Console.Write("Enter your name: ");
+        var name = Console.ReadLine();
+        greeting(name);
+
+    }
+}
