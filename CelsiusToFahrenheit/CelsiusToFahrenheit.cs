7fe5a7adefecec0ac65dc25f31b9e0172c713d93
pkindalov <panayotkindalov@gmail.com>
2016-02-09 22:34:50 +0200

    Simple console converter from celsius to fahrenheit

---
+++ CelsiusToFahrenheit/CelsiusToFahrenheit.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+
+//class CelsiusToFahrenheit
+
+//{
+//    static void Main()
+//    {
+
+//        var celsius = double.Parse(Console.ReadLine());
+
+//        var celciusToF = (celsius * 9) / 5 + 32;
+
+//        Console.WriteLine(Math.Round(celciusToF, 2));
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+class CelsiusToFahrenheit
+{
+    static double CelsiusToFahrenheitConv(double celsius)
+    {
+        var celciusToF = (celsius * 9) / 5 + 32;
+        return Math.Round(celciusToF, 2);
+    }
+
+    static void Main()
+    {
+        var celsius = double.Parse(Console.ReadLine());
+
+        var result = CelsiusToFahrenheitConv(celsius);
+        Console.WriteLine(result);
+    }
+}
