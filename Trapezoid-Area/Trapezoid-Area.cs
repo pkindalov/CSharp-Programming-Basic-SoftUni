18811f0711fe7489da61cd3d32927e6c3111f584
pkindalov <panayotkindalov@gmail.com>
2016-02-09 17:56:48 +0200

    Calculate area of trapezoid. Read necessery data from console and calculate the area of trapezoid. More simple variant is on comment. In second variant I use my own method

---
+++ Trapezoid-Area/Trapezoid-Area.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+
+//class TrapezoidArea
+//{
+//    static void Main()
+//    {
+//        var b1 = double.Parse(Console.ReadLine());
+//        var b2 = double.Parse(Console.ReadLine());
+//        var h = double.Parse(Console.ReadLine());
+//        var area = (b1 + b2) * h / 2;
+//        Console.WriteLine("Trapezoid area = {0}", area);
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+class TrapezoidArea
+{
+    static double TrapezoidAreaCalc(double b1, double b2, double h)
+    {
+        var area = (b1 + b2) * h / 2;
+        return area;
+    }
+
+    static void Main()
+    {
+        var b1 = double.Parse(Console.ReadLine());
+        var b2 = double.Parse(Console.ReadLine());
+        var h = double.Parse(Console.ReadLine());
+      
+        Console.WriteLine("Trapezoid area = {0}", TrapezoidAreaCalc(b1, b2, h));
+    }
+}
+
