cf6e3ecb1c5c70324b06bb5eadacaeae618c48a1
Panayot Kindalov <panayotkindalov@gmail.com>
2016-06-23 10:53:04 +0300

    Graphical application - Cath the button

---
+++ CatchTheButton/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CatchTheButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCatchMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCatchMe
+            // 
+            this.btnCatchMe.Location = new System.Drawing.Point(70, 73);
+            this.btnCatchMe.Name = "btnCatchMe";
+            this.btnCatchMe.Size = new System.Drawing.Size(130, 64);
+            this.btnCatchMe.TabIndex = 0;
+            this.btnCatchMe.Text = "Catch Me!";
+            this.btnCatchMe.UseVisualStyleBackColor = true;
+            this.btnCatchMe.Click += new System.EventHandler(this.btnCatchMe_Click);
+            this.btnCatchMe.MouseEnter += new System.EventHandler(this.btnCatchMe_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCatchMe);
+            this.Name = "Form1";
+            this.Text = "Cath the Button !";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCatchMe;
+    }
+}
+
---
+++ CatchTheButton/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchTheButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCatchMe_MouseEnter(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            var maxWidth = this.Width - btnCatchMe.ClientSize.Width;
+            var maxHeight = this.Height - btnCatchMe.ClientSize.Height;
+            this.btnCatchMe.Location = new Point(rand.Next(maxWidth), rand.Next(maxHeight));
+           
+            
+
+        }
+
+        private void btnCatchMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congratulations! You win!");
+        }
+
+        
+
+
+
+
+    }
+}
